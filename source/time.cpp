0909de242da64c80e207788635d9b1a77920eec6
Konrad <winek353@gmail.com>
2024-03-30 21:07:21 +0100

    upgrade time print

--- source/time.cpp
+++ source/time.cpp
@@ -8,6 +8,6 @@ void measureTime(std::string description, std::function<void()> operationToMeasu
     operationToMeasure();
     auto elapsed = std::chrono::high_resolution_clock::now() - start;
     long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
-    std::cout << "Operation: '" << description << "' has taken " << microseconds << " microseconds" << std::endl;
+    std::cout << "Operation: '" << description << "' has taken " << (microseconds/1000) << "." << (microseconds/100%10) << (microseconds/10%10) << (microseconds%10) << " miliseconds" << std::endl;
 }
 
